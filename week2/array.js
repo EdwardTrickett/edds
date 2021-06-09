3a8a44bc7befc5f997dabdab8e3b6a44b1f620d6
edwardtrickett1983@gmail.com <85286261+EdwardTrickett@users.noreply.github.com>
2021-06-09 15:03:49 +0100

    commit3

---
+++ week2/array.js
@@ -0,0 +1,10 @@
+let veg = ["Broccolli", "Onion", "Tomato"];
+let fruit = ["Apple", "Pear", "Orange"];
+const vegbutton =() => {
+for (let i = 0; i < veg.length; i++){
+console.log(veg[i]);
+}
+}
+vegbutton();
+vegbutton();
+vegbutton();
